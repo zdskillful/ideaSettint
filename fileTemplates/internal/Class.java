f566718f6b0f27bdaa7caf94bd28f2254de9c343
dan.zhang <dan.zhang@lkcoffee.com>
2024-03-27 15:07:58 +0800

    IU-2020.1 <zhangdan@zhangdandeMacBook-Pro-2.local Create baseRefactoring.xml, markdown.xml, vim_settings.xml, ide.general.xml, git_toolbox_2.xml, project.default.xml, rainbow_brackets.xml, diff.xml, Enum.java, Interface.java, macOS copy.xml, lombok-plugin.xml, git.xml, fastRequest.xml, yiiguxing.translation.xml, Default.xml, ignore.xml, MybatisCodeHelperPro.xml, find.xml, one_dark_config.xml, ui.lnf.xml, filetypes.xml, colors.scheme.xml, KeyPromoterXSettings.xml, path.macros.xml, databaseDrivers.xml, mavenVersion.xml, templates.xml, macOS copy 1.xml, OGNL _Struts 2_.xml, Class.java, jdk.table.xml, laf.xml, MyBatisCodeHelper.xml, vcs.xml, grazie_global.xml, toStringTemplates.xml, keymap.xml, Eclipse _macOS_ copy.xml, OGNL.xml, editor.xml, usageView.xml, debugger.xml, Eclipse _macOS_ copy 1.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * @author dan.zhang
+ * @since ${DATE} ${TIME}
+ */ 
+public class ${NAME} {
+}
